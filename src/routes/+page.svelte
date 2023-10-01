c2b0e83745c43337178280669f7f28756960d391
Suteemon <suteemon.team@newton.ac.th>
2023-10-01 09:27:09 +0700

    fixed anim

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,12 @@
 
 <div class="flex items-center justify-center h-screen  ">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<div class="flex flex-row anim-title" transition:fly={{ y: 120, duration: 500 }} >
+		<div class="flex flex-row anim-title" transition:fly={{ y: 120, duration: 600 }} >
 			<h2 class="h2 font-semibold">Hello there, I'm Team</h2>
 			<p class="h2 ml-2 transition ease-in-out duration-300 hover:rotate-45 hover:scale-125">👋🏻</p>
 		</div>
 		
-		<div class="anim-buttons" transition:fly={{ y: 120, duration: 500 }}>
+		<div class="anim-buttons" transition:fly={{ y: 120, duration: 600 }}>
 			<div id="Btns" class="flex flex-col space-y-8 sm:flex-row sm:space-y-0 ">
 				<a href="https://portfolio.teamrr.live" class="flex mx-4 xs:mx-0 justify-center gap-2 px-4 py-2 font-medium transition duration-200 text-gray-900 bg-white border border-gray-200 rounded-xl hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-zinc-800 dark:text-gray-400 dark:border-zinc-800 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"><svg class="w-5 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20">
 					<path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M1 10c1.5 1.5 5.25 3 9 3s7.5-1.5 9-3m-9-1h.01M2 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1ZM14 5V3a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v2h8Z"/>
