2bbf6701468fe473f985199d53978a6b63cfed7d
Suteemon <suteemon.team@newton.ac.th>
2023-10-30 10:40:59 +0700

    change url in projects page

--- src/components/ProjectsPage.svelte
+++ src/components/ProjectsPage.svelte
@@ -4,5 +4,5 @@
 
 <div class="flex flex-col gap-4 lg:flex-row items-center justify-center h-screen" >
     <ProjectsList title={"TimeTabs"} description={"This is my first side-project which is made for students at school to view their class schedule. "} link={"https://timetabs.teamrr.live"} repo={"https://github.com/teammrr/timetabs-react"} />
-    <ProjectsList title={"Book-It"} description={"Small side project which I've intended to provide convenience for student to book a meeting room at school. "} link={"https://timetabs.teamrr.live"} repo={"https://github.com/teammrr/timetabs-react"} />
+    <ProjectsList title={"Book-It"} description={"Small side project which I've intended to provide convenience for student to book a meeting room at school. "} link={"https://bookit.teamrr.live"} repo={"https://github.com/teammrr/room-booking"} />
 </div>
